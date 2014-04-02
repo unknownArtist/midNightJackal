ec669337138621861d9eb75be9cba0441f946176
Muhammad Saqib <rooott@gmail.com>
2014-04-02 13:31:11 +0500

    grunt file changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,6 +59,7 @@ module.exports = function (grunt) {
     // *->    put other dependencies here   <-*
 
     // All of the rest of your app scripts imported here
+    'linker/js/jquery.js',
     'linker/**/*.js'
   ];
 
