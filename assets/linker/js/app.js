0ed4f375e9084abe5354dfa41482f0eefed3fa5c
Muhammad Saqib <rooott@gmail.com>
2014-04-08 19:17:30 +0500

    updates

--- assets/linker/js/app.js
+++ assets/linker/js/app.js
@@ -38,8 +38,8 @@
     // the Sails.js server.
     ///////////////////////////////////////////////////////////
     log(
-        'Socket is now connected and globally accessible as `socket`.\n' + 
-        'e.g. to send a GET request to Sails, try \n' + 
+        'Socket is now connected and globally accessible as `socket`.\n' +
+        'e.g. to send a GET request to Sails, try \n' +
         '`socket.get("/", function (response) ' +
         '{ console.log(response); })`'
     );
